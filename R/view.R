c31b7c33e7b01dc559f633a86bfaf3a72b9801b7
mattdneal <matt@mattdneal.com>
2018-08-16 17:32:43 +0100

    Added prettyFAIMSPlot to plot using levelplot with axes and scale.

--- R/view.R
+++ R/view.R
@@ -44,3 +44,54 @@ to use a better palette.')
   invisible(NULL)
 }
 
+
+#' Plot FAIMS data matrices with axes and ion current scale
+#'
+#' @param dataMatrix a matrix of FAIMS data
+#' @param rowToPlot the row to plot
+#' @param runToPlot the run to plot
+#'
+#' @return NULL
+#' @import lattice
+#' @importFrom grid grid.text
+#' @export
+prettyFAIMSPlot <- function(FAIMSObject, rowToPlot, runToPlot=1, title="") {
+  if (!requireNamespace("viridis", quietly = TRUE)) {
+    warning('Base-R colour palettes are not colour-blind friendly and transfer
+            poorly to greyscale. Install viridis [install.packages("viridis")]
+            to use a better palette.')
+    color.palette <- topo.colors(512)
+  } else {
+    color.palette <- viridis::plasma(512)
+  }
+
+  if (!inherits(FAIMSObject, "FAIMS")) stop("FAIMSObject must inherit class FAIMS")
+  dataMatrix <- FAIMSObject$data
+  faimsDim <- FAIMSObject$faimsDim
+  plotMatrix <- abs(matrix(dataMatrix[rowToPlot, ], nrow=faimsDim[1])[, 1:faimsDim[2] + (runToPlot - 1) * faimsDim[2]])
+
+  zlim <- range(plotMatrix)
+  col.at <- seq(zlim[1], zlim[2], length.out = 256)
+
+
+  colnames(plotMatrix) <- seq(0, 100, length.out = faimsDim[2])
+  rownames(plotMatrix) <- round(seq(-6, 6, length.out=faimsDim[1]), 2)
+  rownames(plotMatrix)[round(seq(1, faimsDim[1], length.out=5))] <- c(-6, -3, 0, 3, 6)
+
+  print(levelplot(plotMatrix,
+            aspect=1,
+            col.regions=color.palette,
+            at=col.at,
+            contour=F,
+            cuts=256,
+            pretty=T,
+            xlab="Compensation Voltage (V)",
+            ylab="Dispersion Field (%)",
+            scales=list(x=list(at=round(seq(1, faimsDim[1], length.out=5))),
+                        y=list(at=round(seq(1, faimsDim[2] ,length.out=6)))),
+            main=title,
+            useRaster=T))
+  trellis.focus("legend", side="right", clipp.off=TRUE, highlight=FALSE)
+  grid::grid.text("Ion count\n(arb. units)", 0.2, -0.02, hjust=0.35, vjust=1.1)
+  trellis.unfocus()
+}
